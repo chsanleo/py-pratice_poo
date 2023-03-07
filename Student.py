af0cb0a6a0afaf73ad973e59dcca924f8ee4ab8b
Christian <chsanleo@gmail.com>
2023-03-07 12:07:46 +0100

    add methods

--- Student.py
+++ Student.py
@@ -6,3 +6,13 @@ class Student():
     years_old = 0
     score = 0
 
+    # Methods
+    def saludar (self):
+        print(f"Hola, me llamo {self.name}")
+    
+    def birthday (self):
+        self.years_old +=1
+
+    def add_score (self, score):
+        if(score >= 0 and score <= 10):
+            self.score = score

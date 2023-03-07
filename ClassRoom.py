82b8e3602ee30ab73b68ed714142a7fc8fb969c9
Christian <chsanleo@gmail.com>
2023-03-07 12:31:13 +0100

    split classes

---
+++ ClassRoom.py
@@ -0,0 +1,11 @@
+from Student import Student
+
+class ClassRoom():
+    students = []
+
+    def show_students(self):
+        for student in self.students:
+            print (student.name)
+
+    def add_student(self, student):
+        self.students.append(student)

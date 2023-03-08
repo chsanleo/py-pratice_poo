5c3fa984c27ae91828d9d7a20c5ab13ddeccb2f2
Christian <chsanleo@gmail.com>
2023-03-08 03:01:49 +0100

    update main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Student import Student
 from ClassRoom import ClassRoom
 
+"""
+# Kata I y Kata II
 student1 = Student("Ethan","Zhang","dni",12)
 student2 = Student("Retro","Machine","dni2",13)
 
@@ -9,3 +11,12 @@ classA.add_student(student1)
 classA.add_student(student2)
 
 classA.show_students()
+"""
+
+# Kata III
+studentNames = ["Ethan", "Retro", "Maria"]
+subjectNames = ["Matematicas", "Lengua", "Sociales"]
+
+classB = ClassRoom(studentNames, subjectNames)
+
+classB.show_classInfo()

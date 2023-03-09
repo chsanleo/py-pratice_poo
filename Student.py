835f3584f2064259da2beec4a53fe8e4e9c3214a
Christian <chsanleo@gmail.com>
2023-03-09 16:57:18 +0100

    changes student class

--- Student.py
+++ Student.py
@@ -2,23 +2,39 @@ class Student():
     # Attributes
     name = ""
     surname = ""
-    dni = ""
-    years_old = 0
+    __dni = ""
+    __years_old = 0
     subject = []
 
     # Constructor
     def __init__ (self, name, surname, dni, years_old):
         self.name = name
         self.surname = surname
-        self.dni = dni
-        self.years_old = years_old
+        self.__dni = dni
+        self.__years_old = years_old
+
+    #Getters/Setters
+    @property
+    def dni (self):
+        return self.__dni
+
+    @property
+    def years_old(self):
+        return self.__years_old
+
+    @years_old.setter
+    def years_old (self, years_old):
+        if(years_old > 0 and years_old < 100):
+            self.__years_old = years_old
+        else:
+            raise TypeError("Edad fuera de rango")
 
     # Methods
     def saludar (self):
         print(f"Hola, me llamo {self.name}")
     
     def birthday (self):
-        self.years_old +=1
+        self.__years_old +=1
 
     def add_subject(self, subject):
         self.subject.append(subject)

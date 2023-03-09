43614996bcf9a2c9cb76c83e37de61264bb5f0a0
Christian <chsanleo@gmail.com>
2023-03-09 17:49:09 +0100

    adapt student with User

--- Student.py
+++ Student.py
@@ -1,40 +1,11 @@
-class Student():
-    # Attributes
-    name = ""
-    surname = ""
-    __dni = ""
-    __years_old = 0
-    subject = []
+from User import User
 
-    # Constructor
+class Student(User):
+
+    subjects = []
+
     def __init__ (self, name, surname, dni, years_old):
-        self.name = name
-        self.surname = surname
-        self.__dni = dni
-        self.__years_old = years_old
-
-    #Getters/Setters
-    @property
-    def dni (self):
-        return self.__dni
-
-    @property
-    def years_old(self):
-        return self.__years_old
-
-    @years_old.setter
-    def years_old (self, years_old):
-        if(years_old > 0 and years_old < 100):
-            self.__years_old = years_old
-        else:
-            raise TypeError("Edad fuera de rango")
-
-    # Methods
-    def saludar (self):
-        print(f"Hola, me llamo {self.name}")
-    
-    def birthday (self):
-        self.__years_old +=1
+        super().__ini__(name, surname, dni, years_old)
 
     def add_subject(self, subject):
         self.subject.append(subject)

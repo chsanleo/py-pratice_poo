d682e22d6ed7d3533f0a3fc677e91ee3d83cec99
Christian <chsanleo@gmail.com>
2023-03-09 17:48:51 +0100

    create class Teacher

---
+++ Teacher.py
@@ -0,0 +1,7 @@
+from User import User
+
+class Teacher(User):
+    speciality = ""
+
+    def __init(self, name, surname, dni, years_old):
+        return super().__init(name, surname, dni, years_old)

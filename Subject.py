397037ef00f9a581693ec9104d2764e9e7ad79f5
Christian <chsanleo@gmail.com>
2023-03-09 16:57:24 +0100

    changes subject class

--- Subject.py
+++ Subject.py
@@ -1,13 +1,24 @@
 class Subject():
     # Properties
+    __id = 0
     name = ""
-    score = 0
+    __score = 0
 
     # Contructor
     def __init__(self, name):
         self.name = name
 
+    #Getters / Setters
+    @property
+    def score(self):
+        return self.__score
+
+    @property
+    def id(self):
+        return self.__id
+    
+
     # Methods
     def add_score (self, score):
         if(score >= 0 and score <= 10):
-            self.score = score
+            self.__score = score
